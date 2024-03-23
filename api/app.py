0c8a5617616d380a7aabb6849e47da1ab8548f96
Tejasvini Chawla <73694047+TejasviniChawla@users.noreply.github.com>
2024-03-23 21:40:00 +0530

    Updated file for vercel hosting

--- api/app.py
+++ api/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request
-from example import add2
-from ml import add
+from api.example import add2
+from api.ml import add
 
 app= Flask(__name__)
 
